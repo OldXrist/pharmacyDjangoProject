24f7a508e80f757eb641c9c1df47b8679b31ce41
Igor <104066443+OldXrist@users.noreply.github.com>
2024-05-31 01:38:31 +0300

    Signing & Signup pages update

--- account/forms.py
+++ account/forms.py
@@ -2,7 +2,15 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 # Form details for user while creating
+
+
 class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+
+        labels = {
+            "username": "Введите логин",
+            "password1": "Введите пароль",
+            "password2": "Повторите пароль",
+        }
